2264ea8ab7185a4cb6398b1fc1dffb9a0193bec7
andrsand <angorko@list.ru>
2023-11-12 18:55:27 +0300

    Creating a Project File Structure

---
+++ src/screens/ScreenSetting.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+export const ScreenHome = () => {
+    return (
+        <View>
+            <Text>ScreenHome</Text>
+        </View>
+    );
+};
